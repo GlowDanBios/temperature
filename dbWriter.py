a894b57a82fefcfd46358daf2cab6517efa9392a
Universai <solomatin377@gmail.com>
2020-02-29 17:41:55 +0300

    1

--- dbWriter.py
+++ dbWriter.py
@@ -1,4 +1,24 @@
-def  writeap(cid,aid,hid,fid,temp):#TODO Матяшу запись в бд температуру из квартир
+from pymongo import MongoClient
 
+def  writeap(cid,aid,hid,fid,temp):
+    client = MongoClient('mongodb://Matiash:mth@45.8.230.173')
+    db = client.get_database("gdms")
+    col = db.get_collection("temp")
+    post = {
+        "cid": cid,
+        "aid": aid,
+        "hid": hid,
+        "fid": fid,
+        "temp": temp
+    }
+    col.insert(post)
 
-def writecit(cid,temp):#TODO Матяшу записать в бд температуру в городе
+def writecit(cid,temp):
+    client = MongoClient('mongodb://Matiash:mth@45.8.230.173')
+    db = client.get_database("gdms")
+    col = db.get_collection("temp-c")
+    post = {
+        "cid": cid,
+        "temp-c": temp
+    }
+    col.insert(post)
