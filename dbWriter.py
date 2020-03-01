a0ed7a95eced77a94a308f75d80d99fc0ff99265
Universai <solomatin377@gmail.com>
2020-03-01 10:03:50 +0300

    2

--- dbWriter.py
+++ dbWriter.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
+import datetime
 
 def  writeap(cid,aid,hid,fid,temp):
     client = MongoClient('mongodb://Matiash:mth@45.8.230.173')
     db = client.get_database("gdms")
     col = db.get_collection("temp")
     post = {
+        "time": datetime.datetime.now(),
         "cid": cid,
         "aid": aid,
         "hid": hid,
@@ -18,6 +20,7 @@ def writecit(cid,temp):
     db = client.get_database("gdms")
     col = db.get_collection("temp-c")
     post = {
+        "time": datetime.datetime.now(),
         "cid": cid,
         "temp-c": temp
     }
