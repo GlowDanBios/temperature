c6dc84ba5acda697fb5a03deea8721318764ae05
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2020-03-03 11:33:39 +0300

    уменьшил кол-во

--- collector.py
+++ collector.py
@@ -13,7 +13,7 @@ def getaps():
         respa = requests.get(adr+str(cit['city_id']), headers=headers).json()
         for are in range(1,respa['data']['area_count']):
             resph = requests.get(adr+str(cit['city_id'])+"/"+str(are),headers=headers).json()
-            for i in range(15):
+            for i in range(10):
                 resfin = requests.get(adr + str(cit['city_id'])+"/"+str(are) + "/"+str(resph['data'][i]['house_id'])+"/"+'1',headers=headers).json()
                 t = float(resfin['data']["temperature"])
 
