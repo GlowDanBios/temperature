0fb36cab538030d1a7480f03c3875ef242325ddd
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2020-03-03 09:58:28 +0300

    уменьшил кол-во

--- collector.py
+++ collector.py
@@ -3,24 +3,29 @@ from dbWriter import writeap,writecit
 import time
 
 adr = "http://dt.miet.ru/ppo_it/api/"
-token = ""
+token = "6gjgr2u0mqzzx8hm"
+headers = {'X-Auth-Token': token}
 
 
 def getaps():
-    respc = requests.get(adr).text
+    respc = requests.get(adr,headers=headers).json()
     for cit in respc['data']:
-        respa = requests.get(adr+":"+cit['city_id']).text
-        for are in range(respa['area_count']):
-            resph = requests.get(adr+":"+cit['city_id'+":"+str(are)).text
-            for hous in range(resph['house_id']):
-                respap = requests.get(adr+":"+cit['city_id'+":"+str(are)+":"+str(hous)).text
-                for apart in range(respap["apartment_count"]):
-                    resfin = requests.get(adr + ":" + cit['city_id'] + ":" + str(are) + ":" + str(hous)+":"+str(apart)).text
-                    t = float(resfin["temperature"])
-                    write(cit['city_id'],are,hous,apart,t)
+        respa = requests.get(adr+str(cit['city_id']), headers=headers).json()
+        for are in range(1,respa['data']['area_count']):
+            resph = requests.get(adr+str(cit['city_id'])+"/"+str(are),headers=headers).json()
+            for hous in resph['data']:
+                resfin = requests.get(adr + str(cit['city_id'])+"/"+str(are) + "/"+str(hous['house_id'])+"/"+'1',headers=headers).json()
+                t = float(resfin['data']["temperature"])
+
+                #writeap(cit['city_id'], are, hous['house_id'], 1, t)
+                time.sleep(0.3)
+
 
 def getcit():
-    respc = requests.get(adr).text
+    respc = requests.get(adr, headers=headers).text
     for cit in respc['data']:
-        resp = respa = requests.get(adr+":"+cit['city_id']).text
+        resp = respa = requests.get(adr+"/:"+cit['city_id'],headers=headers).text
         writecit(cit['city_id'], resp['temperature'])
+
+
+getaps()
