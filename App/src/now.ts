12ed7a785d9d0d25f6d76fb30cca9b9827273e95
Ваше Имя <you@example.com>
2020-03-10 10:20:07 +0300

    now

--- App/src/now.ts
+++ App/src/now.ts
@@ -1,4 +1,3 @@
-//Romanenko suka tut napishet code s vunkcieei getNow()
 async function now(toFind: {
     cid: number,
     aid: number,
@@ -9,14 +8,53 @@ async function now(toFind: {
     var headers = {
         'X-Auth-Token': '6gjgr2u0mqzzx8hm'
     }
-    var res = (await (await fetch(url, {
+    return (await (await fetch(url, {
         headers
     })).json()).data
-    //еби этот резалт полностью
 }
-now({
-    cid: 1,
-    aid: 1,
-    hid: 1,
-    fid: 1
+const cities = {
+    'Алмазный': 1,
+    'Восточный': 2,
+    'Западный': 3,
+    'Заречный': 4,
+    'Курортный': 5,
+    'Лесной': 6,
+    'Научный': 7,
+    'Полярный': 8,
+    'Портовый': 9,
+    'Приморский': 10,
+    'Садовый': 11,
+    'Северный': 12,
+    'Степной': 13,
+    'Таёжный': 14,
+    'Центральный': 15,
+    'Южный': 16
+}
+var select = document.getElementById('id');
+Object.keys(cities).forEach(el => {
+    let opt = document.createElement('option');
+    opt.value = el;
+    opt.innerHTML = el;
+    select.add(opt);
 })
+
+let sec = document.getElementById('id2');
+for (let i = 1; i <= 15; i += 1) {
+    let option = document.createElement('option');
+    option.value = option.text = i;
+    sec.add(option);
+}
+async function main() {
+    const MongoClient = require("mongodb").MongoClient;
+    const mongoClient = new MongoClient("mongodb://Matiash:mth@45.8.230.173", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    document.getElementById("temp").innerHTML += `<h1>${await now({
+        cid: cities[document.getElementById("id").value],
+        aid: Number(document.getElementById("id1").value),
+        hid: Number(document.getElementById("id2").value),
+        fid: 1 //////////////////////////////////////////////////доделай плучание fid из селекта
+    })}</h1>`
+    client.close()
+}
